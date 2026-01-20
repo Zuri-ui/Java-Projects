22fd262247c0c061a97732fd811dfab7c17d8dc0
Princess Afia Achiaa <nachiaa586@gmail.com>
2026-01-20 09:43:40 +0000

    Initial commit

---
+++ Maximum_Finder.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class MaximumFinder
+{
+    Scanner input = new Scanner(System.in);
+    System.out,print("Enter three numbers seperated by space: ");
+    int number1 = input.nextInt();
+    int number2 = input.nextInt();
+    int number3 = input.nextInt();
+
+    int result = maximum(number1, number2, number3);
+
+    System.out.print("Maximum is:  " + result);
+
+}
+`
+{
+    public static integer maximum(number1, number2, number3);//
+    
+  
+}
